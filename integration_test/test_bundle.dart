0046a3f2f25ce054282718a98afcb84fd1bdc045
mihirj <mhrjsh1@gmail.com>
2025-09-28 18:11:43 +0530

    feat(test): add patrol test for iOS with actual test

--- integration_test/test_bundle.dart
+++ integration_test/test_bundle.dart
@@ -9,7 +9,7 @@ import 'package:patrol/src/native/contracts/contracts.dart';
 import 'package:test_api/src/backend/invoker.dart';
 
 // START: GENERATED TEST IMPORTS
-import 'example_test.dart' as example_test;
+import 'presentation/home/home_page_test.dart' as presentation__home__home_page_test;
 // END: GENERATED TEST IMPORTS
 
 Future<void> main() async {
@@ -69,7 +69,7 @@ Future<void> main() async {
   });
 
   // START: GENERATED TEST GROUPS
-  group('example_test', example_test.main);
+  group('presentation.home.home_page_test', presentation__home__home_page_test.main);
   // END: GENERATED TEST GROUPS
 
   final dartTestGroup = await testExplorationCompleter.future;
